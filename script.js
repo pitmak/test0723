c55437031e19eab2d1bd11fe2b8830032de539af
pitmak <piter_makarov@mail.ru>
2023-07-28 11:41:48 +0500

    fix

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+const sdk = await new AppExtensionsSDK().initialize();
+
 const formEl = document.querySelector('form');
 
-formEl.addEventListener('submit', (e) => {
+formEl.addEventListener('submit', async (e) => {
   e.preventDefault();
   console.log('on submit form');
+  await sdk.execute(Command.CLOSE_MODAL);
 });
