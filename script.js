944bc8249c3222b390e0c7a8efed4ca05c841438
pitmak <piter_makarov@mail.ru>
2023-07-28 12:05:00 +0500

    fix

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const sdk = await new AppExtensionsSDK().initialize();
-await sdk.execute(Command.RESIZE, { height: 500, width: 900 });
+await sdk.execute('resize', { height: 500, width: 900 });
 
 const formEl = document.querySelector('form');
 
 formEl.addEventListener('submit', async (e) => {
   e.preventDefault();
   console.log('on submit form');
-  //await sdk.execute(Command.CLOSE_MODAL);
-  await sdk.execute(Command.REDIRECT_TO, { view: View.DEALS });
+  await sdk.execute('close_modal');
+  //await sdk.execute(Command.REDIRECT_TO, { view: View.DEALS });
 });
